25447b6600c2d590fbcaab52e00eda0ce2fe6064
Sergio <tojosergiodaniel@gmail.com>
2026-06-20 11:48:39 -0300

    actualizaciones de package

--- src/controllers/ProductController.java
+++ src/controllers/ProductController.java
@@ -1,32 +1,32 @@
-package controllers;
-
-import interfaces.Controller;
-import models.Request;
-import utils.Product;
-
-public class ProductController implements Controller {
-
-    @Override
-    public String procesar(Request request) {
-
-        String action = request.getAccion();
-
-        Product product = (Product) request.getParams();
-
-        switch(action.toLowerCase()) {
-
-            case "alta":
-
-                return
-                    "Alta del producto: " + product;
-            case "baja":
-                return
-                    "Baja del producto "+product;
-            case "editar":
-                return
-                	"Editar producto "+ product;
-            default:
-            	throw new RuntimeException("Accion no soportada");
-        }
-    }
-}
+	package controllers;
+	
+	import interfaces.Controller;
+	import models.Request;
+	import utils.Product;
+	
+	public class ProductController implements Controller {
+	
+	    @Override
+	    public String procesar(Request request) {
+	
+	        String action = request.getAccion();
+	
+	        Product product = (Product) request.getParams();
+	
+	        switch(action.toLowerCase()) {
+	
+	            case "alta":
+	
+	                return
+	                    "Alta del producto: " + product;
+	            case "baja":
+	                return
+	                    "Baja del producto "+product;
+	            case "editar":
+	                return
+	                	"Editar producto "+ product;
+	            default:
+	            	throw new RuntimeException("Accion no soportada");
+	        }
+	    }
+	}
