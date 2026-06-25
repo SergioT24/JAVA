57e2e2fdbefecbdc75aeb2ff5ab79d229a28087c
SergioT24 <80526173+SergioT24@users.noreply.github.com>
2026-06-25 15:55:15 -0300

    Refactor: construcción de Main2
    Agrego la construcción e inicialización de la app registrando y ensamblando controllers, parsers, etc.

--- src/Mains/Main2.java
+++ src/Mains/Main2.java
@@ -1,17 +1,28 @@
 package Mains;
-
 import builders.NiglioAppBuilder;
 import models.NiglioApp;
 
 public class Main2 {
-	public static void main(String[] args) {
-	
-	
-	NiglioAppBuilder builder = new NiglioAppBuilder();
-	
-	
-	
-	
-	
+		public static void main(String[] args) {
+		// Construcción e inicialización completa de la aplicación
+			NiglioApp app = new NiglioAppBuilder()
+					// Registro de recursos y sus respectivos controladores/parsers
+					.addController("producto", new ProductController())
+					.addParser("producto", new ProductParser())
+				
+					.addController("auto", new CarController())
+					.addParser("auto", new CarParser())
+				
+					// Inicialización del administrador de usuarios
+					.useUserManager(new DefaultUserManager())
+				
+					// Configuración del medio de comunicación (Consola / Socket)
+					.useConsole()
+				
+					// Construcción final de la aplicación
+					.build();
+			
+			// Inicio de la aplicación
+			app.start();
+		}
 	}
-}
